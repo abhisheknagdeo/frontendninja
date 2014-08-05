4bc42cf730d6af846a1faedee90865812a38484f
Sandeep Choudhari <sandeep_choudhari@persistent.co.in>
2014-08-05 19:01:58 +0530

    Pending tasks commit

---
+++ gitproxy.bat
@@ -0,0 +1,5 @@
+@echo off
+set http_proxy=http://abhishek_nagdeo%40persistent.co.in:Sadguru%40123@ngproxy.persistent.co.in:8080
+set https_proxy=http://abhishek_nagdeo%40persistent.co.in:Sadguru%40123@ngproxy.persistent.co.in:8080
+echo GIT proxy set.
+echo on
